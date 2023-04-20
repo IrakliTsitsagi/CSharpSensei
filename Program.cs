17e63cc1e7aa74b43f0be64163bf3474084a7a86
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-20 12:44:52 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int num3;
+
+            Console.WriteLine("enter number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            for (int i = 2; i < number; i++)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3);
+                num1 = num2;
+                num2 = num3;
+                if (num1 + num2 >= number)
+                    break;
+            }
+        }
+    }
+}
