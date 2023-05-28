f680f9fa43c61ab54c24e7f87237160601a3b08f
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-28 23:57:38 +0400

    Add files via upload

---
+++ newfile/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using qenirame;
+
+namespace persona
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public string sex { get; set; }
+    }
+}
---
+++ newfile/Qenirame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using persona;
+namespace qenirame
+{
+    public class Qenirame
+    {
+        public static void chawere(Person obj)
+        {
+            string file = @"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Males\Names.txt";  // ese
+            StreamWriter sw = new StreamWriter(file, true);
+            sw.WriteLine($"{obj.name},{obj.surname},{obj.age},{obj.sex}");
+            sw.Close();
+
+
+
+        }
+
+        public static string waikitxe()
+        {
+
+            string fileReader = @"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Males\Names.txt";
+            StreamReader sr = new StreamReader(fileReader);
+            string wakitxuli = "";
+            wakitxuli = sr.ReadLine();
+            // Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+            return wakitxuli;
+        }
+    }
+}
