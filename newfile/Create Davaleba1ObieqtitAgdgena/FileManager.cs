d29f76d581f08b8576b456dcc7468c88fa887a0f
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-01 15:55:29 +0400

    Add files via upload

---
+++ newfile/Create Davaleba1ObieqtitAgdgena/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Persona;
+namespace File;
+
+public class FileManager
+{
+
+    public string Path { get; set; }
+    public void Write(Person obj)
+    {
+
+        StreamWriter sw = new StreamWriter(Path, true);
+        sw.WriteLine($"{obj.Name},{obj.Surname},{obj.Age},{obj.Sex}");
+
+        sw.Close();
+
+
+
+    }
+
+    public Person Read()
+    {
+        StreamReader sr = new StreamReader(Path);
+        string readInfo = sr.ReadLine();
+        string[] array = readInfo.Split(',');
+
+        Person Recovered = new Person();
+        Recovered.Name = array[0];
+        Recovered.Surname = array[1];
+        Recovered.Age = int.Parse(array[2]);
+        Recovered.Sex = array[3];
+
+        // Console.WriteLine(sr.ReadToEnd());
+        sr.Close();
+        return Recovered;
+    }
+
+    public FileManager(string path)
+    {
+        Path = path;
+    }
+}
---
+++ newfile/Create Davaleba1ObieqtitAgdgena/SenseiDavaleba1.cs
@@ -0,0 +1,35 @@
+﻿using Persona;
+using File;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+
+namespace SenseiDavaleba1
+{
+    internal class SenseiDavaleba1
+    {
+        static void Main(string[] args)
+        {
+            Person Dejan = new Person();
+            Dejan.Name = "Dejan";
+            Dejan.Surname = "Stankovic";
+            Dejan.Age = 44;
+            Dejan.Sex = "Male";
+
+            FileManager manager = new FileManager(@"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Males\Names.txt");
+
+            manager.Write(Dejan);
+
+            Person recovered = manager.Read();
+            Console.WriteLine($"{recovered.Name} {recovered.Surname} {recovered.Age} {recovered.Sex}");
+
+            //Directory.CreateDirectory(@"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Males");
+            //Directory.CreateDirectory(@"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Females");
+
+            //sw.WriteLine($"person name is {Dejan.name}, surname is {Dejan.surname}, age  {Dejan.age}, sex {Dejan.sex}");
+            //sw.Close();
+
+
+        }
+    }
+}
