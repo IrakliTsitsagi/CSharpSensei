0ed8408d709dce93ffdb3f3491270a113d48c867
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-03 15:34:10 +0400

    Add files via upload

---
+++ newfile/SenseiDavalebaArrayPersons/FileManager.cs
@@ -0,0 +1,74 @@
+﻿using Persona;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace File;
+
+public class FileManager
+{
+
+    public string Path { get; set; }
+
+    public void Write(Person[] persons)
+    {
+        StreamWriter sw = new StreamWriter(Path, true);
+
+        foreach (Person person in persons)
+        {
+            sw.WriteLine(person.ToString());
+        }
+
+        sw.Close();
+
+    }
+
+    public Person[] Read()
+    {
+        Person[] recovered = new Person[3];
+
+        StreamReader sr = new StreamReader(Path);
+
+        string line;
+        int counter = 0;
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] splitPersonProperties = line.Split(',');
+
+            //recovered[counter] = new Person();
+
+            //recovered[counter].Name = splitPersonProperties[0].Trim();
+            //recovered[counter].Surname = splitPersonProperties[1].Trim();
+            //recovered[counter].Age = int.Parse(splitPersonProperties[2].Trim());
+            //recovered[counter].Sex = splitPersonProperties[3].Trim();
+
+            //uketesi gza
+            recovered[counter] = CreatePersonFromStringArray(splitPersonProperties);
+
+            counter++;
+            Array.Resize(ref recovered, recovered.Length+1);
+        }
+        
+        sr.Close();
+
+        return recovered;
+    }
+
+    private Person CreatePersonFromStringArray(string[] properties)
+    {
+        Person person = new Person();
+
+        person.Name = properties[0].Trim();
+        person.Surname = properties[1].Trim();
+        person.Age = int.Parse(properties[2].Trim());
+        person.Sex = properties[3].Trim();
+
+        return person;
+    }
+
+    public FileManager(string path)
+    {
+        Path = path;
+    }
+}
+
+
---
+++ newfile/SenseiDavalebaArrayPersons/SenseiDavaleba1.cs
@@ -0,0 +1,63 @@
+﻿using Persona;
+using File;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Globalization;
+
+namespace SenseiDavaleba1
+{
+    internal class SenseiDavaleba1
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] Persons = new Person[3];
+
+            //new Person{ Name = "Dejan", Surname = "Stankovic", Age = 25, Sex = "Male"},
+            // new Person { Name = "Bob", Surname = "Big", Age = 30 , Sex= "Male"},
+            // new Person { Name = "Vaja", Surname = "Wiklauri", Age = 40000, Sex = "Male" }
+            Persons[0] = new Person();
+            Persons[0].Name = "Dejan";
+            Persons[0].Surname = "Stankovic";
+            Persons[0].Age = 44;
+            Persons[0].Sex = "Male";
+
+            
+            Persons[1] = new Person();
+            Persons[1].Name = "Ala";
+            Persons[1].Surname = "Pugachova";
+            Persons[1].Age = 90;
+            Persons[1].Sex = "Female";
+
+            Persons[2] = new Person();
+            Persons[2].Name = "Vaja";
+            Persons[2].Surname = "Wiklauri";
+            Persons[2].Age = 40000000;
+            Persons[2].Sex = "Male";
+
+
+            FileManager manager = new FileManager(@"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Males\Names.txt");
+                manager.Write(Persons);
+
+            Person[] Recovered = manager.Read();
+            for (int i = 0; i < Recovered.Length; i++)
+            {
+                Console.WriteLine(Recovered[i]);
+
+            }
+
+
+            // Person recovered = manager.Read();
+            //Console.WriteLine($"{recovered.Name} {recovered.Surname} {recovered.Age} {recovered.Sex}");
+
+            //Directory.CreateDirectory(@"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Males");
+            //Directory.CreateDirectory(@"C:\Users\\Irakli\OneDrive\Desktop\C#\Sensei\Females");
+
+            //sw.WriteLine($"person name is {Dejan.name}, surname is {Dejan.surname}, age  {Dejan.age}, sex {Dejan.sex}");
+            //sw.Close();
+
+
+        }
+    }
+}
