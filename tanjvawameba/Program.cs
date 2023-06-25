6020ef9f99547e00944bfcef4444e5b1f3fcb188
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-06-25 15:33:03 +0400

    Add files via upload

---
+++ tanjvawameba/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptografiaWameba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encryption encrypt = new Encryption();
+            Decryption decrypt = new Decryption();
+            string filePath = @"C:\Users\Irakli\OneDrive\Desktop\C#\test\kriptoo.txt";
+            WriteAndRead write = new WriteAndRead(filePath);
+
+            int shift = 3;
+            int choice = -1;
+            string code = "";
+            string message = "";
+
+
+            while (choice != 0)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Read");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter code word: ");
+                        code = Console.ReadLine();
+                        Console.Write("Enter your message: ");
+                        message = Console.ReadLine();
+                        string encryptedText = Encryption.Encrypt(message, shift);
+                        string decryptedText = Decryption.Decrypt(encryptedText, shift);
+
+
+                        WriteAndRead.Write(filePath, encryptedText);
+                        Console.WriteLine("Message encrypted and written to the file.");
+                        WriteAndRead.SaveCodeToFile(code);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter code word to read: ");
+                        string codeAgain = Console.ReadLine();
+                        code = WriteAndRead.LoadCodeFromFile();
+                        if (codeAgain == code)
+                        {
+                            string encryptedMessage = WriteAndRead.Read(filePath);
+                            string decryptedMessage = Decryption.Decrypt(encryptedMessage, shift);
+                            Console.WriteLine("Decrypted message: " + decryptedMessage);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Code was wrong, decryption failed.");
+                        }
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Exiting the program...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ tanjvawameba/WriteAndRead.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CryptografiaWameba
+{
+    public class WriteAndRead
+    {
+        public string Path { get; set; }
+
+        public static void Write(string path, string encryptedText)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write(encryptedText);
+            }
+        }
+
+        public static string Read(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllText(filePath);
+            }
+
+            return "";
+        }
+
+        public WriteAndRead(string path)
+        {
+            Path = path;
+        }
+
+        public static string LoadCodeFromFile()
+        {
+            string codeFilePath = @"C:\Users\Irakli\OneDrive\Desktop\C#\test\code.txt";
+
+            if (File.Exists(codeFilePath))
+            {
+                return File.ReadAllText(codeFilePath);
+            }
+
+            return "";
+        }
+
+        public static void SaveCodeToFile(string code)
+        {
+            string codeFilePath = @"C:\Users\Irakli\OneDrive\Desktop\C#\test\code.txt";
+            File.WriteAllText(codeFilePath, code);
+        }
+    }
+}
+
+
